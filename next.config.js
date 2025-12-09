577b3008a7861585d24d75d10e985c8a8384b63c
mlm272 <maggielouisemayer@gmail.com>
2025-12-09 15:57:06 -0500

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/maggiemayer-portfolio' : '',
   images: {
     unoptimized: true,
     remotePatterns: [],
